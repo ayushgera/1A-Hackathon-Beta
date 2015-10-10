57be2d99f8aa6c37ff90273842077060ec113e86
Ayush Gera <ayush.gera@amadeus.com>
2015-10-11 03:15:14 +0530

    map

--- www/js/index.js
+++ www/js/index.js
@@ -48,8 +48,8 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        //document.addEventListener('deviceready', this.onDeviceReady, false);
-		this.onDeviceReady();
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+		//this.onDeviceReady();
     },
     // deviceready Event Handler
     //
